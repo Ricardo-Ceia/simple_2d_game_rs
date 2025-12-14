e64211d1e99cc62a49a618bb249bad15b375b898
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-12-14 23:29:45 +0000

    function to spawn aliens

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-use macroquad::{miniquad::native::linux_x11::libx11::Screen, prelude::*};
+use macroquad::prelude::*;
+
+fn generate_enemy_pos(x_limit: f32) -> f32 {
+    rand::gen_range(0.0, x_limit)  
+}
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
     
     let rect_width = 120.0;
-    let rect_height = 60.0;
-    let rect_y = 100.0;
+    let rect_height = 10.0;
+    let rect_y = screen_height() - 100.0;
     let speed = 3.0;
 
     let mut rect_pos_x = 0.0;
     let mut movement = 1.0;
-
+    
+    let mut enemy_pos_x;
     loop {
         let screen_w = screen_width();
+
+        enemy_pos_x = generate_enemy_pos(screen_w);
+        
+        println!("Alien position->{enemy_pos_x}");
 
         rect_pos_x += speed*movement;
 
@@ -21,7 +30,7 @@ async fn main() {
         }
         
         draw_rectangle(rect_pos_x, rect_y, rect_width, rect_height, GREEN);
-
+        draw_fps();
         next_frame().await;
     }
 }
